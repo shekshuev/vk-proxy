e793939a566acf24a01cb7694d8020461c1c6221
Sergei Shekshuev <39078883+shekshuev@users.noreply.github.com>
2022-10-28 12:59:41 +0300

    route changes

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    vk_api_url = "https://api.vk.com"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    vk_api_url = "https://api.vk.com/" + path
+    print(vk_api_url)
     try:
         return requests.get(vk_api_url, params=request.args).content
     except Exception as e:
         return {
             "error": str(e)
         }
-
-
+    
