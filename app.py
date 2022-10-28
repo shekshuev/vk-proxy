70315a3f5b94a0e5da1c925ab813aab253d616bc
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2022-10-28 15:04:20 +0300

    app changes

--- app.py
+++ app.py
@@ -7,15 +7,13 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    vk_api_url = "https://api.vk.com/" + path
-    print(vk_api_url)
+    vk_api_url = "https://api.vk.com/method/" + path
     try:
         return requests.get(vk_api_url, params=request.args).content
     except Exception as e:
         return {
             "error": str(e)
         }
-    
-    
+
 if __name__ == "__main__":
-    app.run()
+    app.run("0.0.0.0")
